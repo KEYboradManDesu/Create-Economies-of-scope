bd957779112a1b50c2c2d1b400a618d86dfa80fe
Gl0w1amp <blockmasterzero@gmail.com>
2023-10-14 11:56:57 +0800

    v0.2.0-pre_1

--- kubejs/server_scripts/scripts/mechisms/mechisms.js
+++ kubejs/server_scripts/scripts/mechisms/mechisms.js
@@ -1,27 +1,36 @@
 // 此文件未完成
 onEvent('recipes', event => {
     kinetic(event) //动力
-    seal(event) //密封
-    sturdy(event) //坚实
-    infernal(event) //酷热
-    abstruse(event) //谐振
-    calcu(event) //计算
-    inductive(event) //感应
-    g_source(event) //金源
-    meta(event) //异构
+    //seal(event) //密封
+    //sturdy(event) //坚实
+    //infernal(event) //酷热
+    //abstruse(event) //谐振
+    //calcu(event) //计算
+    //inductive(event) //感应
+    //g_source(event) //金源
+    //meta(event) //异构
 })
+
+// 此处初始化变量
+var output_item
+var trans_item
+var success_chance
+var fail_chance
+var input_item
+var cons_item
+var rand_item
 
 // 以下配方可直接复制替换
 // 开始注册动力构件
 function kinetic(event){
-    var output_item = 'kubejs:kinetic_mechanism' //此处修改输出
-    var trans_item = 'kubejs:incomplete_kinetic_mechanism'
+    output_item = 'kubejs:kinetic_mechanism' //此处修改输出
+    trans_item = 'kubejs:incomplete_kinetic_mechanism'
     // 初级合成，40%成功率，共一个配方 
-    var success_chance = 40.0
-    var fail_chance = 60.0
-    var input_item = 'kubejs:pen_regis_01' //此处修改输入
-    var cons_item = 'create:andesite_alloy'
-    var rand_item = 'kubejs:missingno'
+    success_chance = 40.0
+    fail_chance = 60.0
+    input_item = 'kubejs:pen_regis_01' //此处修改输入
+    cons_item = 'create:andesite_alloy'
+    rand_item = 'kubejs:missingno'
 
     event.recipes.createSequencedAssembly([
         //成品：
@@ -39,11 +48,11 @@ function kinetic(event){
    ]).transitionalItem(trans_item)
 
     // 中级合成，60%成功率，共3个配方
-    var success_chance = 60.0
-    var fail_chance = 40.0
-    var input_item = 'kubejs:pen_regis_02' //此处修改输入
-    var rand_item = 'kubejs:missingno'
-    var cons_item = 'kubejs:andesite_alloy_gear'
+    success_chance = 60.0
+    fail_chance = 40.0
+    input_item = 'kubejs:pen_regis_02' //此处修改输入
+    rand_item = 'kubejs:missingno'
+    cons_item = 'kubejs:andesite_alloy_gear'
 
     // 安山齿轮
     event.recipes.createSequencedAssembly([
@@ -62,8 +71,8 @@ function kinetic(event){
    ]).transitionalItem(trans_item)
 
    // 机械手锯
-    var input_item = 'kubejs:pen_regis_03'
-    var cons_item = 'create:andesite_alloy'
+    input_item = 'kubejs:pen_regis_03'
+    cons_item = 'create:andesite_alloy'
     event.recipes.createSequencedAssembly([
         //成品：
         Item.of(output_item).withChance(success_chance),
@@ -80,7 +89,7 @@ function kinetic(event){
    ]).transitionalItem(trans_item)
 
    // 模板装配
-    var input_item = 'tconstruct:pattern'
+    input_item = 'tconstruct:pattern'
     event.recipes.createSequencedAssembly([
         //成品：
         Item.of(output_item).withChance(success_chance),
@@ -97,11 +106,11 @@ function kinetic(event){
    ]).transitionalItem(trans_item)
 
     // 高级合成，80%成功率，共3个配方
-    var success_chance = 80.0
-    var fail_chance = 20.0
-    var input_item = 'tconstruct:pattern' //此处修改输入
-    var rand_item = 'kubejs:missingno'
-    var cons_item = 'kubejs:andesite_alloy_gear'
+    success_chance = 80.0
+    fail_chance = 20.0
+    input_item = 'tconstruct:pattern' //此处修改输入
+    rand_item = 'kubejs:missingno'
+    cons_item = 'kubejs:andesite_alloy_gear'
 
     // 模板+齿轮
     event.recipes.createSequencedAssembly([
@@ -120,7 +129,7 @@ function kinetic(event){
    ]).transitionalItem(trans_item)
 
    // 模板+机械手锯
-    var cons_item = 'create:andesite_alloy'
+    cons_item = 'create:andesite_alloy'
     event.recipes.createSequencedAssembly([
         //成品：
         Item.of(output_item).withChance(success_chance),
@@ -137,7 +146,7 @@ function kinetic(event){
    ]).transitionalItem(trans_item)
 
    // 齿轮+手锯
-    var input_item = '#minecraft:wooden_slabs'
+    input_item = '#minecraft:wooden_slabs'
     event.recipes.createSequencedAssembly([
         //成品：
         Item.of(output_item).withChance(success_chance),
@@ -154,8 +163,8 @@ function kinetic(event){
    ]).transitionalItem(trans_item)
 
     // 终级合成，100%成功率，共1个配方
-    var input_item = 'tconstruct:pattern' //此处修改输入
-    var cons_item = 'kubejs:andesite_alloy_gear'
+    input_item = 'tconstruct:pattern' //此处修改输入
+    cons_item = 'kubejs:andesite_alloy_gear'
 
     // 模板+齿轮+手锯
     event.recipes.createSequencedAssembly([

157222bdb872655ba8e871831ba0f9d596330240
Gl0w1amp <blockmasterzero@gmail.com>
2024-01-17 09:52:34 +0800

    Try to fix vegetable soup bug

--- kubejs/startup_scripts/launch.js
+++ kubejs/startup_scripts/launch.js
@@ -214,6 +214,7 @@ event.create('unarmed_missile', 'create:sequenced_assembly').texture("kubejs:ite
 
 event.create('kubejs:incomplete_steel_facing', 'create:sequenced_assembly').texture("kubejs:item/cans/steel_facing").displayName('钢牙套（未完成）')
 event.create('steel_facing').texture("kubejs:item/cans/steel_facing").displayName('钢牙套')
+
 // 罐装食品
 event.create('iron_thin_plate').texture("kubejs:item/cans/iron_thin_plate").displayName('薄铁板')
 event.create('iron_thin_plate_with_tin').texture("kubejs:item/cans/iron_thin_plate_with_tin").displayName('镀锡薄铁板')
@@ -235,7 +236,7 @@ event.create('can_sealed_vegetable_soup').texture("kubejs:item/cans/can_sealed")
 event.create('can_opened_vegetable_soup').texture("kubejs:item/cans/can_opened").displayName('蔬菜汤罐头')
 	.food((food) => {
 		food.hunger(14)
-		food.saturation(15)
+		food.saturation(4)
 		.meat(false)
 		.alwaysEdible(true)
 		.fastToEat(true)
